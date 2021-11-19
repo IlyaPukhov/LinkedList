4d6de6fb1b99ce62d3c09f0b4e20ab4358db9aa3
Ilya Pukhov <IlyaPukhov@github.com>
2021-11-19 21:41:47 +0300

    LinkedList

---
+++ src/main/kotlin/LinkedList.kt
@@ -0,0 +1,35 @@
+class LinkedList<T> {
+    var head: Node<T>? = null
+    var tail: Node<T>? = null
+
+
+    /** Добавление элемента в голову связного списка. */
+    fun push(x: T) {
+        val node = Node(x)
+        if (head == null) {   //если список пуст, то голова и хвост - это один узел
+            head = node
+            tail = node
+        } else {
+            head = Node(x, head) //новая голова теперь ссылается на "бывшую" первую
+        }
+    }
+
+    /** Добавление элемента в хвост связного списка. */
+    fun addTail(x: T) {
+        val node = Node(x)
+        if (head == null) {    //если список пуст, то голова и хвост - это один узел
+            head = node
+            tail = node
+        } else {
+            tail?.next = node  //иначе "старый" последний элемент теперь ссылается на новый
+            tail = node        //а в указатель на последний элемент записываем адрес нового элемента
+        }
+    }
+
+    /** Удаление элемента из головы связного списка. */
+    fun pop(): T? {
+        val value = head?.value
+        head = head?.next
+        return value
+    }
+}
